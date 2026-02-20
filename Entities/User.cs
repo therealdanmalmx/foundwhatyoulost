af9a3d1ea41d6c0fb7c299087b95bcbc8056b425
therealdanmalmx <danmalmx@gmail.com>
2026-02-20 09:13:03 +0100

    Change record to class for inheritance

--- Entities/User.cs
+++ Entities/User.cs
@@ -1,7 +1,16 @@
-public record User (
-    Guid Id,
-    string Name,
-    string Email,
-    string Phone,
-    Guid? ItemId
-);
+using Microsoft.AspNetCore.Identity;
+
+public  class User : IdentityUser
+{
+    public Guid? ItemId { get; set; }
+
+    public User() {}
+    public User(string Id, string userName, string phoneNumber, string email, Guid? itemId)
+    {
+        this.Id = Id;
+        UserName = userName;
+        PhoneNumber = phoneNumber;
+        Email = email;
+        ItemId = itemId;
+    }
+}
