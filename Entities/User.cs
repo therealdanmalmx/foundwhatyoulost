8c53d2046e18ad64327d46aa5d3d20005b515665
therealdanmalmx <danmalmx@gmail.com>
2026-02-24 09:15:15 +0100

    Add name in constructor

--- Entities/User.cs
+++ Entities/User.cs
@@ -2,12 +2,14 @@ using Microsoft.AspNetCore.Identity;
 
 public  class User : IdentityUser
 {
+    public string? Name { get; set; }
     public Guid? ItemId { get; set; }
 
     public User() {}
-    public User(string Id, string userName, string phoneNumber, string email, Guid? itemId)
+    public User(string Id, string name, string userName, string phoneNumber, string email, Guid? itemId)
     {
         this.Id = Id;
+        Name = name;
         UserName = userName;
         PhoneNumber = phoneNumber;
         Email = email;
