56e8af952aea738e1ac77a873a24e8edb474ee32
therealdanmalmx <danmalmx@gmail.com>
2026-03-02 09:10:27 +0100

    Use identity DB context

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -2,11 +2,12 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace foundWhatYouLost.Data
 {
-public class AppDbContext : DbContext
+public class AppDbContext : IdentityDbContext<User>
 {
     public AppDbContext(DbContextOptions<AppDbContext> options): base(options) {}
 
